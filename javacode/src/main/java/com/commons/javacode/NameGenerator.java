397b745a81d0a455b2044c0a6573517e70f0736b
guangchaowu <guangchaowu@sohu-inc.com>
2018-01-19 16:24:55 +0800

    add

---
+++ javacode/src/main/java/com/commons/javacode/NameGenerator.java
@@ -0,0 +1,72 @@
+package com.sohu.spaces.videos.photo.storage;
+
+import java.net.InetAddress;
+
+/**
+ * Key Generator, 产生图片唯一id,作为文件名
+ *
+ * @author FWX
+ * @version $Id: NameGenerator.java,v 1.4 2008/06/27 09:12:46 xiegang Exp $
+ */
+public class NameGenerator {
+    private static final long IP;
+    private static final int SIP;
+    static {
+        long ipadd;
+        byte bip;
+        try {
+            ipadd = toLong(InetAddress.getLocalHost().getAddress());
+            bip = InetAddress.getLocalHost().getAddress()[3];
+        } catch (Exception e) {
+            ipadd = 0;
+            bip = 0;
+        }
+        IP = ipadd;
+        if (bip < 0) {
+            SIP = bip + 256;
+        } else {
+            SIP = bip;
+        }
+    }
+
+    private static short counter = (short) 0;
+    private static final int JVM = (int) (System.currentTimeMillis() >>> 8);
+
+    private NameGenerator() {
+    }
+
+    /**
+     * 输出字符串
+     *
+     * @return
+     */
+    public static String generateName(String passportUID) {
+        long keyValue = IP + JVM + getCount() + System.currentTimeMillis();
+        if (passportUID != null && !"".equals(passportUID))
+            return passportUID + "_" + Long.toHexString(keyValue) + "g" + SIP;
+        else
+            return Long.toHexString(keyValue) + "g" + SIP;
+    }
+
+    private static short getCount() {
+        synchronized (NameGenerator.class) {
+            if (counter < 0) {
+                counter = 0;
+            }
+            return counter++;
+        }
+    }
+
+    /**
+     * @param bytes
+     *            byte[]
+     * @return int
+     */
+    private static long toLong(byte[] bytes) {
+        long result = 0;
+        for (int i = 0; i < bytes.length; i++) {
+            result = (result << 8) - Byte.MIN_VALUE + bytes[i];
+        }
+        return result;
+    }
+}
