73272f1b50e953e01897af539ac119cc12efeb28
guangchaowu <guangchaowu@sohu-inc.com>
2018-10-30 16:24:46 +0800

    b2

--- javacode/src/test/java/com/T1.java
+++ javacode/src/test/java/com/T1.java
@@ -5,5 +5,6 @@ public class T1 {
     public static void main(String[] args) {
         System.out.println("aaaabbbcccdddeee");
         System.out.println("dd");
+        System.out.println("111");
     }
 }
