5ebc92f7b6059fcb42e2da0060f06148e04fb01a
guangchaowu <guangchaowu@sohu-inc.com>
2018-10-30 16:25:41 +0800

    a4

--- javacode/src/test/java/com/T1.java
+++ javacode/src/test/java/com/T1.java
@@ -4,7 +4,7 @@ public class T1 {
 
     public static void main(String[] args) {
         System.out.println("aaaabbbcccdddeee");
-        System.out.println("dd");
+        System.out.println("ddeee");
         System.out.println("111");
         System.out.println("www");
     }
