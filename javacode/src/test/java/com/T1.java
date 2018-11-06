f553fd14733c8ab5b677b4c8d36f11fe38447c06
guangchaowu <guangchaowu@sohu-inc.com>
2018-11-06 14:24:00 +0800

    Squashed commit of the following:
    commit debfb4041eb27cc25a6cf8008dc9bc79e3004e56
    Author: lsxqingtom <lsxqing@qq.com>
    Date:   Tue Nov 6 14:16:13 2018 +0800
    
        Update T1.java
    
    commit ee06e301028736550bb24d5eb7020d6a2e997674
    Author: lsxqingtom <lsxqing@qq.com>
    Date:   Tue Nov 6 14:15:58 2018 +0800
    
        Update T1.java

--- javacode/src/test/java/com/T1.java
+++ javacode/src/test/java/com/T1.java
@@ -6,6 +6,6 @@ public class T1 {
         System.out.println("aaaabbbcccdddeee");
         System.out.println("ddeeeff");
         System.out.println("111");
-        System.out.println("wwweeffggab");
+        System.out.println("wwweeffggabddd");
     }
 }
