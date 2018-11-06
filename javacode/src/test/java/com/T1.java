0fcf69394411855de0039d258e61f62219489086
lsxqingtom <lsxqing@qq.com>
2018-11-06 14:11:06 +0800

    Test1 1 (#1)
    * a1
    
    * a2
    
    * a3
    
    * a4
    
    * a5
    
    * b1
    
    * b2
    
    * b3
    
    * a4
    
    * a5
    
    * b4
    
    * b5
    
    * a6
    
    * 1

---
+++ javacode/src/test/java/com/T1.java
@@ -0,0 +1,12 @@
+package com;
+
+public class T1 {
+
+    public static void main(String[] args) {
+        System.out.println("aaaabbbcccdddeee");
+        System.out.println("ddeeeff");
+        System.out.println("111");
+        System.out.println("wwweeffggfff");
+        System.out.println("");
+    }
+}
