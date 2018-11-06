ee06e301028736550bb24d5eb7020d6a2e997674
lsxqingtom <lsxqing@qq.com>
2018-11-06 14:15:58 +0800

    Update T1.java

--- javacode/src/test/java/com/T1.java
+++ javacode/src/test/java/com/T1.java
@@ -6,6 +6,6 @@ public class T1 {
         System.out.println("aaaabbbcccdddeee");
         System.out.println("ddeeeff");
         System.out.println("111");
-        System.out.println("wwweeffgg");
+        System.out.println("wwweeffgga");
     }
 }
