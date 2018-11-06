1044f4b3ace10fc923b7692bf9da8f42b1fe4b48
guangchaowu <guangchaowu@sohu-inc.com>
2018-11-06 14:30:10 +0800

    ddddddd

--- javacode/src/test/java/com/T1.java
+++ javacode/src/test/java/com/T1.java
@@ -6,6 +6,6 @@ public class T1 {
         System.out.println("aaaabbbcccdddeee");
         System.out.println("ddeeeff");
         System.out.println("111");
-        System.out.println("wwweeffggabdddff");
+        System.out.println("wwweeffggabdddffq2");
     }
 }
