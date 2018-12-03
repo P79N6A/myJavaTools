87773c92ca62e23f886fa6280aa44807ada195fe
guangchaowu <guangchaowu@sohu-inc.com>
2018-12-04 01:08:42 +0800

    44444

--- javacode/src/test/java/com/T1.java
+++ javacode/src/test/java/com/T1.java
@@ -7,7 +7,7 @@ public class T1 {
         System.out.println("ddeeeff");
         System.out.println("111");
         System.out.println("wwweeffggabdddffq2344567");
-        System.out.println("aabbcc");
+        System.out.println("aabbccdd");
     }
 
     public static void aa(){
