8682f9050501db5fd2e09eae4047f614cf892c7f
guangchaowu <guangchaowu@sohu-inc.com>
2019-01-25 18:33:10 +0800

    update

---
+++ javacode/src/test/java/com/T1.java
@@ -0,0 +1,16 @@
+package com;
+
+public class T1 {
+
+    public static void main(String[] args) {
+        System.out.println("aaaabbbcccdddeee");
+        System.out.println("ddeeeff");
+        System.out.println("111");
+        System.out.println("wwweeffggabdddffq2344567");
+        System.out.println("aabbccddeefffff");
+    }
+
+    public static void aa(){
+
+    }
+}
