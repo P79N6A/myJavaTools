60640cb68a6ecd6286c46a80f34b05c520e4e903
guangchaowu <guangchaowu@sohu-inc.com>
2019-01-25 18:35:45 +0800

    # Conflicts: #	javacode/src/test/java/com/T1.java
    # Conflicts:
    #	javacode/src/test/java/com/T1.java
    
    # Conflicts:
    #	javacode/src/test/java/com/T1.java

--- javacode/src/test/java/com/T1.java
+++ javacode/src/test/java/com/T1.java
@@ -7,7 +7,7 @@ public class T1 {
         System.out.println("ddeeeff");
         System.out.println("111");
         System.out.println("wwweeffggabdddffq2344567");
-        System.out.println("aabbccddeefffff");
+        System.out.println("aabbccdd");
     }
 
     public static void aa(){
