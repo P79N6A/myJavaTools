4c591f5e73c277f797c2bcdf99da9faf3a22845c
guangchaowu <guangchaowu@sohu-inc.com>
2019-04-26 16:48:50 +0800

    update

--- javacode/src/main/java/com/commons/javacode/ThreadPoolUtils.java
+++ javacode/src/main/java/com/commons/javacode/ThreadPoolUtils.java
@@ -2,12 +2,20 @@
  * Copyright (c) 2013 uhos. All Rights Reserved
  */
 package com.commons.javacode;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * <p>
  * Description: 
  * </p>
  */
-public class ThreadPoolExecutor {
+public class ThreadPoolUtils {
+
+    //方法一，spring bean方式
     /*
     <bean id="taskExecutor"
             class="org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor">
@@ -16,7 +24,6 @@ public class ThreadPoolExecutor {
         </bean>
         */
     public void test(){
-        
         taskExecutor.execute(new Runnable(){
 
             public void run() {
@@ -25,7 +32,16 @@ public class ThreadPoolExecutor {
             
         });
     }
-    
+
+
+    //方法二：
+    //同一时间只允许一个任务运行，当有任务运行时，新来的任务直接丢弃掉
+    private static Executor pool = new ThreadPoolExecutor(1, 1,
+            0L, TimeUnit.MILLISECONDS,
+            new ArrayBlockingQueue<Runnable>(1),new ThreadPoolExecutor.DiscardOldestPolicy());
+
+
+
 }
 
     
---
+++ scriptcode/src/shell/service.sh
@@ -0,0 +1,17 @@
+function stopService(){
+    MAIN_CLASS=$1
+    SERVER_PID=`ps auxf | grep ${MAIN_CLASS} | grep -v "grep"| awk '{print $2}'`
+    echo "server pid is [${SERVER_PID}]"
+    for pid in ${SERVER_PID}
+    do
+        kill -9 $pid
+        echo "$pid is killed!"
+    done
+}
+
+
+
+
+
+
+
