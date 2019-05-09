25fbecf2c3cd5a62ce566b3b126f9e481f61c5ac
guangchaowu <guangchaowu@sohu-inc.com>
2019-05-09 23:00:35 +0800

    add xmlutils

--- javacode/src/main/java/com/commons/xml/jaxb/VideoMeta.java
+++ javacode/src/main/java/com/commons/xml/jaxb/VideoMeta.java
@@ -10,9 +10,7 @@ import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Date;
 
-/**
- * @author xiegang video meta info
- */
+
 @Entity
 @Table(name = "video_meta")
 public class VideoMeta implements Serializable {
