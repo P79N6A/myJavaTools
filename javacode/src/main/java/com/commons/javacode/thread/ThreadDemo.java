262292777169a1058c7306e5f038ec8ee1c5c670
guangchaowu <guangchaowu@sohu-inc.com>
2019-07-26 23:00:16 +0800

    update

--- javacode/src/main/java/com/commons/javacode/thread/ThreadDemo.java
+++ javacode/src/main/java/com/commons/javacode/thread/ThreadDemo.java
@@ -9,10 +9,18 @@ public class ThreadDemo {
 
     public static void main(String[] args){
         //最多只能有一个线程
+        //方一
         ThreadPoolExecutor pool = new ThreadPoolExecutor(1, 1,
                 60L, TimeUnit.SECONDS,
                 new SynchronousQueue<Runnable>(), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
-
+        //方二
+        //下面也可以是newFixedThreadPool，newCachedThreadPool
+        ThreadPoolExecutor taskPool=(ThreadPoolExecutor)Executors.newSingleThreadExecutor();
+        int threadCount = taskPool.getActiveCount();
+        if(threadCount>0){
+            System.out.println("last task isruning...");
+            return;
+        }
     }
 
 }
